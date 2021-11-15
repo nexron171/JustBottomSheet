837b0edd2604e33dcf502c2c87f3f4c27ffbcd63
Sergey Shirnin <nexron171@gmail.com>
2021-11-15 12:15:10 +0300

    Fixed integration tests

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -7,27 +7,61 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button to open bottom sheet',
-        (WidgetTester tester) async {
+    testWidgets('tap on the floating action button to open bottom sheet', (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
+      final fab = await _openBottomSheet(tester);
 
-      final Finder fab = find.byTooltip('Increment');
+      final draggableZone = find.byKey(const Key("DraggableZone"));
+      expect(fab.hitTestable(), findsNothing, reason: "FAB should be invisible");
+      expect(draggableZone, findsOneWidget, reason: "Bottom sheet should be opened");
+    });
 
-      await tester.tap(fab);
+    testWidgets('fling on draggable zone to close bottom sheet', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      final fab = await _openBottomSheet(tester);
 
+      final draggableZone = find.byKey(const Key("DraggableZone"));
+      expect(fab.hitTestable(), findsNothing, reason: "FAB should be invisible");
+      expect(draggableZone, findsOneWidget, reason: "Bottom sheet should be opened");
+
+      await tester.fling(draggableZone, const Offset(0, 50), 500);
+      await tester.pumpAndSettle();
+      expect(draggableZone.hitTestable(), findsOneWidget);
+
+      await tester.fling(draggableZone, const Offset(0, 50), 1500);
       await tester.pumpAndSettle();
 
-      var draggableZone = find.byKey(const ValueKey("DraggableZone"));
-      await tester.fling(draggableZone, const Offset(50, 100), 200);
-      await tester.pumpAndSettle();
-      await tester.ensureVisible(draggableZone);
+      expect(fab.hitTestable(), findsOneWidget, reason: "FAB should be visible");
+    });
 
-      await Future.delayed(const Duration(seconds: 1));
-      await tester.fling(draggableZone, const Offset(50, 100), 800);
+    testWidgets('fling on scroll to close bottom sheet', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      final fab = await _openBottomSheet(tester);
+
+      final scrollableZone = find.byKey(const Key("ScrollBox"));
+      expect(fab.hitTestable(), findsNothing, reason: "FAB should be invisible");
+      expect(scrollableZone, findsOneWidget, reason: "Bottom sheet should be opened");
+
+      await tester.timedDrag(scrollableZone, const Offset(0, 200), const Duration(milliseconds: 500));
+      await tester.pumpAndSettle();
+      expect(scrollableZone.hitTestable(), findsOneWidget);
+
+      await tester.timedDrag(scrollableZone, const Offset(0, 200), const Duration(milliseconds: 100));
       await tester.pumpAndSettle();
 
-      await tester.ensureVisible(fab);
+      expect(fab.hitTestable(), findsOneWidget, reason: "FAB should be visible");
     });
   });
+}
+
+Future<Finder> _openBottomSheet(WidgetTester tester) async {
+  final Finder fab = find.byTooltip('Increment');
+
+  await tester.tap(fab);
+  await tester.pumpAndSettle();
+
+  return fab;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
             context: context,
             closeOnScroll: true,
             cornerRadius: 32,
+            scrollController: scrollController,
             builder: (context) {
               return ListView.builder(
                 controller: scrollController,
@@ -77,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
 Future<T?> showBottomSheet<T>({
   required BuildContext context,
   required Widget Function(BuildContext) builder,
+  required ScrollController scrollController,
   bool closeOnScroll = false,
   Color? backgroundColor,
   Color? handleBarColor,
@@ -85,6 +87,7 @@ Future<T?> showBottomSheet<T>({
   return Navigator.of(context).push<T>(
     JustBottomSheetRoute(
       builder: builder,
+      scrollController: scrollController,
       closeOnScroll: closeOnScroll,
       backgroundColor: backgroundColor,
       handleBarColor: handleBarColor,
@@ -96,6 +99,7 @@ Future<T?> showBottomSheet<T>({
 class JustBottomSheetRoute<T> extends ModalRoute<T> {
   JustBottomSheetRoute({
     required this.builder,
+    required this.scrollController,
     this.closeOnScroll = false,
     this.backgroundColor,
     this.handleBarColor,
@@ -103,6 +107,7 @@ class JustBottomSheetRoute<T> extends ModalRoute<T> {
   }) : super();
 
   final Widget Function(BuildContext) builder;
+  final ScrollController scrollController;
   final bool closeOnScroll;
   final Color? backgroundColor;
   final Color? handleBarColor;
@@ -118,11 +123,11 @@ class JustBottomSheetRoute<T> extends ModalRoute<T> {
   String? get barrierLabel => null;
 
   @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
+  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
     return JustBottomSheetPage(
       height: MediaQuery.of(context).size.height,
       builder: builder,
+      scrollController: scrollController,
       closeOnScroll: closeOnScroll,
       backgroundColor: backgroundColor,
       handleBarColor: handleBarColor,
@@ -131,12 +136,11 @@ class JustBottomSheetRoute<T> extends ModalRoute<T> {
   }
 
   @override
-  Widget buildTransitions(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation, Widget child) {
+  Widget buildTransitions(
+      BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
     var begin = const Offset(0, 1);
     var end = Offset.zero;
-    var tween =
-        Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.easeOut));
+    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.easeOut));
     var offsetAnimation = animation.drive(tween);
 
     return SlideTransition(
@@ -159,6 +163,7 @@ class JustBottomSheetPage extends StatefulWidget {
   const JustBottomSheetPage({
     required this.height,
     required this.builder,
+    required this.scrollController,
     this.closeOnScroll = false,
     this.backgroundColor,
     this.handleBarColor,
@@ -169,6 +174,7 @@ class JustBottomSheetPage extends StatefulWidget {
   final double height;
   final bool closeOnScroll;
   final Widget Function(BuildContext) builder;
+  final ScrollController scrollController;
   final Color? backgroundColor;
   final Color? handleBarColor;
   final double? cornerRadius;
@@ -177,8 +183,14 @@ class JustBottomSheetPage extends StatefulWidget {
   _JustBottomSheetPageState createState() => _JustBottomSheetPageState();
 }
 
-class _JustBottomSheetPageState extends State<JustBottomSheetPage>
-    with TickerProviderStateMixin {
+class _ScrollMetrics {
+  const _ScrollMetrics(this.timeStamp, this.pixels);
+
+  final int timeStamp;
+  final double pixels;
+}
+
+class _JustBottomSheetPageState extends State<JustBottomSheetPage> with TickerProviderStateMixin {
   bool isDragging = false;
   bool isClampingScroll = false;
   bool shouldPop = false;
@@ -189,7 +201,12 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
   double targetYOffset = 0;
   double previousYOffset = 0;
   double scrollPosition = 0;
-  DateTime lastScrollTimeStamp = DateTime.now();
+
+  final double scrollDistanceTolerance = 20;
+  final double velocityToClose = 1500;
+
+  List<_ScrollMetrics> scrollMetrics = [];
+  int lastScrollTimeStamp = 0;
 
   void handleDragUpdates({required double offset, required double delta}) {
     if (offset <= -20) {
@@ -209,11 +226,24 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
   }
 
   double getScrollVelocityFromPixels(double pixels) {
-    return (pixels - scrollPosition).abs() * 20;
+    final currentTimeStamp = DateTime.now().millisecondsSinceEpoch;
+    final duration = currentTimeStamp - lastScrollTimeStamp;
+    lastScrollTimeStamp = currentTimeStamp;
+    if (duration == 0) {
+      return 0;
+    }
+
+    final distance = (pixels - scrollPosition) * -1;
+    if (distance < scrollDistanceTolerance) {
+      return 0;
+    }
+
+    final velocity = distance / (duration / 1000);
+    return velocity;
   }
 
   bool isVelocityEnoughToPop(double velocity) {
-    return velocity >= 500;
+    return velocity >= velocityToClose;
   }
 
   @override
@@ -246,13 +276,10 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
                   isDragging = true;
                 },
                 onVerticalDragUpdate: (details) {
-                  final newYOffset = details.globalPosition.dy -
-                      totalTopPadding -
-                      handlerPadding.top -
-                      handlerHeight / 2;
+                  final newYOffset =
+                      details.globalPosition.dy - totalTopPadding - handlerPadding.top - handlerHeight / 2;
 
-                  handleDragUpdates(
-                      offset: newYOffset, delta: details.delta.dy);
+                  handleDragUpdates(offset: newYOffset, delta: details.delta.dy);
                 },
                 onVerticalDragEnd: (details) {
                   isDragging = false;
@@ -260,8 +287,7 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
 
                   final velocity = details.primaryVelocity ?? 0;
 
-                  shouldPop = shouldPop && isVelocityEnoughToPop(velocity) ||
-                      targetYOffset >= widget.height / 2;
+                  shouldPop = shouldPop && isVelocityEnoughToPop(velocity) || targetYOffset >= widget.height / 2;
 
                   if (shouldPop && !willPop) {
                     willPop = true;
@@ -276,13 +302,12 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
                 child: Column(
                   children: [
                     SizedBox(
-                      key: const ValueKey("DraggableZone"),
+                      key: const Key("DraggableZone"),
                       height: handlerContainerHeight,
                       width: width,
                       child: DecoratedBox(
                         decoration: BoxDecoration(
-                          color: widget.backgroundColor ??
-                              Theme.of(context).canvasColor,
+                          color: widget.backgroundColor ?? Theme.of(context).canvasColor,
                         ),
                         child: Center(
                           child: ClipRRect(
@@ -308,9 +333,7 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
     return Stack(
       children: [
         TweenAnimationBuilder<double>(
-          duration: isDragging || scrollPosition < 0
-              ? Duration.zero
-              : const Duration(milliseconds: 250),
+          duration: isDragging || scrollPosition < 0 ? Duration.zero : const Duration(milliseconds: 250),
           curve: Curves.linearToEaseOut,
           tween: Tween<double>(
             begin: previousYOffset,
@@ -337,23 +360,18 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
           child: backgroundBody,
         ),
         TweenAnimationBuilder<double>(
-          duration:
-              isDragging ? Duration.zero : const Duration(milliseconds: 250),
+          duration: isDragging ? Duration.zero : const Duration(milliseconds: 250),
           curve: Curves.linearToEaseOut,
           tween: Tween<double>(
-            begin: widget.height -
-                totalTopPadding -
-                handlerContainerHeight -
-                (scrollPosition < 0 ? 0 : previousYOffset),
-            end: widget.height -
-                totalTopPadding -
-                handlerContainerHeight -
-                (scrollPosition < 0 ? 0 : targetYOffset),
+            begin:
+                widget.height - totalTopPadding - handlerContainerHeight - (scrollPosition < 0 ? 0 : previousYOffset),
+            end: widget.height - totalTopPadding - handlerContainerHeight - (scrollPosition < 0 ? 0 : targetYOffset),
           ),
           builder: (context, value, child) {
             return Positioned(
               bottom: 0,
               child: SizedBox(
+                key: const Key("ScrollBox"),
                 width: width,
                 height: value,
                 child: Column(
@@ -375,12 +393,9 @@ class _JustBottomSheetPageState extends State<JustBottomSheetPage>
                             if (!widget.closeOnScroll) {
                               return false;
                             }
-                            final velocity = getScrollVelocityFromPixels(
-                                scroll.metrics.pixels);
+                            final velocity = getScrollVelocityFromPixels(scroll.metrics.pixels);
                             if (scroll.metrics.pixels <= 0) {
-                              if (isVelocityEnoughToPop(velocity) &&
-                                  !willPop &&
-                                  isPointerDown) {
+                              if (isVelocityEnoughToPop(velocity) && !willPop && isPointerDown) {
                                 willPop = true;
                                 // Navigator.of(context).pop();
                               }
